df3879668ca559447e50e521e631f9f9fc1cecbf
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2019-03-17 13:49:50 -0400

    split out parse funcitons; score counts on a scale of 1

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import parse
 import requests
 import nltk
 from bs4 import BeautifulSoup
 from pprint import pprint
 from collections import Counter
-from nameparser.parser import HumanName
 
 nltk.download("maxent_ne_chunker")
 nltk.download("words")
@@ -41,64 +41,16 @@ def fetch():
     return abstracts
 
 
-def get_nouns(text):
-    print("collecting nouns...")
-
-    nouns = []
-    stop = nltk.corpus.stopwords.words("english")
-    for item in tagged:
-        if (
-            item[1] == "NN"
-            and item[0].lower() not in stop
-            and item[0].lower().isalpha()
-        ):
-            nouns.append(item[0])
-    return Counter(nouns)
-
-
-def get_verbs(text):
-    print("collecting verbs...")
-
-    verbs = []
-    stop = nltk.corpus.stopwords.words("english")
-    for item in tagged:
-        if (
-            item[1][0] == "V"
-            and item[0].lower() not in stop
-            and item[0].lower().isalpha()
-        ):
-            verbs.append(item[0])
-    return Counter(verbs)
-
-
-def get_proper_names(text):
-    print("collecting proper names...")
-
-    sentt = nltk.ne_chunk(text, binary=False)
-    person_list = []
-    person = []
-    name = ""
-    print("checking names against a dictionary...")
-    for subtree in sentt.subtrees(filter=lambda t: t.label() == "PERSON"):
-        for leaf in subtree.leaves():
-            if not nltk.corpus.wordnet.synsets(leaf[0]):
-                person.append(leaf[0])
-        for part in person:
-            name += part + " "
-        person_list.append(name[:-1])
-        name = ""
-        person = []
-
-    return Counter(person_list)
-
-
 if __name__ == "__main__":
     abstracts = fetch()
     print("parsing parts of speech...")
     tagged = nltk.pos_tag(nltk.word_tokenize(abstracts))
-    nouns = get_nouns(tagged)
-    pprint(nouns.most_common(20))
-    proper_names = get_proper_names(tagged)
-    pprint(proper_names.most_common(20))
-    verbs = get_verbs(tagged)
-    pprint(verbs.most_common(20))
+
+    nouns = parse.get_nouns(tagged)
+    pprint(nouns)
+
+    verbs = parse.get_verbs(tagged)
+    pprint(verbs)
+
+    proper_names = parse.get_proper_names(tagged)
+    pprint(proper_names)
---
+++ parse.py
@@ -0,0 +1,57 @@
+import nltk
+from collections import Counter
+
+
+def get_nouns(text):
+    print("collecting nouns...")
+
+    nouns = []
+    stop = nltk.corpus.stopwords.words("english")
+    for item in text:
+        if (
+            item[1] == "NN"
+            and item[0].lower() not in stop
+            and item[0].lower().isalpha()
+        ):
+            nouns.append(item[0])
+    counter = Counter(nouns).most_common(100)
+    return [(k, v / counter[0][1]) for k, v in counter]
+
+
+def get_verbs(text):
+    print("collecting verbs...")
+
+    verbs = []
+    stop = nltk.corpus.stopwords.words("english")
+    for item in text:
+        if (
+            item[1][0] == "V"
+            and item[0].lower() not in stop
+            and item[0].lower().isalpha()
+        ):
+            verbs.append(item[0])
+    counter = Counter(verbs).most_common(100)
+    return [(k, v / counter[0][1]) for k, v in counter]
+
+
+def get_proper_names(text):
+    print("collecting proper names...")
+
+    sentt = nltk.ne_chunk(text, binary=False)
+    person_list = []
+    person = []
+    name = ""
+    print("checking names against a dictionary...")
+    for subtree in sentt.subtrees(filter=lambda t: t.label() == "PERSON"):
+        for leaf in subtree.leaves():
+            if not nltk.corpus.wordnet.synsets(leaf[0]):
+                person.append(leaf[0])
+        for part in person:
+            name += part + " "
+        if name[:-1]:
+            person_list.append(name[:-1])
+        name = ""
+        person = []
+
+    counter = Counter(person_list).most_common(100)
+    return [(k, v / counter[0][1]) for k, v in counter]
