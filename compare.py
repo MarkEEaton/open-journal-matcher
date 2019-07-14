dca65055dc4516b71e522792890eb1ff9e157ac3
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2019-07-14 15:22:02 -0400

    beginning of wtforms implementation

--- compare.py
+++ compare.py
@@ -6,21 +6,24 @@ import collections
 import requests
 from spacy.tokens import Doc
 from flask import Flask, render_template, request
+from wtforms import Form, StringField, validators
 
 nlp = spacy.load("en_core_web_md")
 comp = {}
 
 app = Flask(__name__)
 
+class RegistrationForm(Form):
+    abstract = StringField('abstract', [validators.length(min=25, max=10000)])
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     """ display index page """
-    if request.method == "GET":
-        return render_template("index.html")
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
 
-    else:
-        abs_data = nlp(abstract)
+        abs_data = nlp(form.abstract.data)
 
         for item in glob.glob("docs/*"):
             print(item)
@@ -42,6 +45,11 @@ def index():
             data = [title, issn, score]
         return render_template("index.html", data=data)
 
+    elif request.method == "POST" and not form.validate():
+        print("failed")
+
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(port=8000, host="127.0.0.1", debug=True)
