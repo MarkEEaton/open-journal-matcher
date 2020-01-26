867cd92d010679924ebe42d02e632ee2d1594da6
Mark Eaton <markeeaton@users.noreply.github.com>
2020-01-26 19:56:48 +0000

    trying to wire up google cloud function. work in progress...

--- compare-trio.py
+++ compare-trio.py
@@ -1,10 +1,12 @@
 """ run the comparisons using trio (async) """
 
 import spacy
+import json
 import trio
 import glob
 import collections
 import requests
+import settings
 from spacy.tokens import Doc
 from datetime import datetime
 
@@ -19,25 +21,26 @@ counter = 0
 t0 = datetime.now()
 
 
-async def parent(counter):
+async def parent(counter, abs_data):
     print("running parent")
     async with trio.open_nursery() as nursery:
-        for item in glob.glob("docs-md/*"):
+        for item in glob.glob("docs-md/*")[:3]:
             counter += 1
-            nursery.start_soon(fileio, item)
+            nursery.start_soon(fileio, item, abs_data)
             print(item, counter)
 
 
-async def fileio(item):
-    print("opening file")
-    with open(item, "rb") as item_data:
-        data = Doc(nlp.vocab).from_bytes(item_data.read())
-        print(abs_data.similarity(data))
-        comp[item[8:]] = abs_data.similarity(data)
+async def fileio(item, abs_data):
+    with open(item, "rb") as i:
+        resp = requests.post(settings.cloud_function, data={"d": [[abs_data]], "e": str(i.read())})
+    data = spacy.tokens.Doc(nlp.vocab).from_bytes(resp.text)
+    print(nlp("test this out").similarity(data))
+    #comp[item[8:]] = score
 
 
-trio.run(parent, counter)
+trio.run(parent, counter, abs_data)
 
+"""
 print("sorting")
 top = sorted(comp.items(), key=lambda x: x[1], reverse=True)[:5]
 
@@ -57,3 +60,4 @@ for item in top:
 
 t1 = datetime.now()
 print(t1 - t0)
+"""
