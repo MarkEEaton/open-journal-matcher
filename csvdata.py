078c3f255d4cc811b63feb62a8f58658bc81c404
Mark Eaton <MarkEEaton@users.noreply.github.com>
2020-08-16 00:15:55 -0500

    add regex verification when parsing csv

--- csvdata.py
+++ csvdata.py
@@ -2,6 +2,7 @@
 
 import csv
 import json
+import regex
 
 output = []
 
@@ -16,9 +17,15 @@ with open("journallist-June2020.csv", newline="") as csvfile:
                 ]  # throw an index error if the field is only 'english', nothing more
         except IndexError:
             if row[4]:
-                output.append(row[4])
+                if regex.match(r'^[0-9]{4}-[0-9]{3}[0-9xX]$', row[4]):
+                    output.append(row[4])
+                else:
+                    print(row[4], 'regex does not match')
             elif row[3]:
-                output.append(row[3])
+                if regex.match(r'^[0-9]{4}-[0-9]{3}[0-9Xx]$', row[3]):
+                    output.append(row[3])
+                else:
+                    print(row[3], 'regex does not match')
             else:
                 print("no issn")
 
