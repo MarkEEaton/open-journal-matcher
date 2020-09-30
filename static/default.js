64e3fc6b24acd95c6a370d33b215964f67fb7c54
Mark Eaton <MarkEEaton@users.noreply.github.com>
2020-09-29 22:42:56 -0400

    fix js bug; use quotes consistently in js

--- static/default.js
+++ static/default.js
@@ -1,23 +1,27 @@
-document.getElementById("show").addEventListener('click', function() {
-    document.getElementById("waiting").style.display = '';
-    document.getElementById("results").style.display = 'none';
-    document.getElementById("docs").style.display = 'none';
-    window.location.hash = 'jump';
+document.getElementById("show").addEventListener("click", function() {
+    document.getElementById("waiting").style.display = "";
+    document.getElementById("results").style.display = "none";
+    document.getElementById("docs").style.display = "none";
+    window.location.hash = "jump";
     history.replaceState(null, null, ' ');
 });      
 
-document.getElementById("finger").addEventListener('click', function() {
-    document.getElementById("docs").style.display = '';
-    document.getElementById("results").style.display = 'none';
-    if (document.getElementById("waiting").style.display === '') {
+document.getElementById("finger").addEventListener("click", function() {
+    document.getElementById("docs").style.display = "";
+    document.getElementById("results").style.display = "none";
+    if (document.getElementById("waiting").style.display === "") {
         document.getElementById("docs").style.margin = 0;
     }
-    window.location.hash = 'jump';
-    history.replaceState(null, null, ' ');
+    window.location.hash = "jump";
+    history.replaceState(null, null, " ");
 });
 
-document.getElementById("hideDocs").addEventListener('click', function() {
-	document.getElementById("docs").style.display = 'none';
-	document.getElementById("results").style.display = '';
-    document.getElementById("error").style.display = 'none';
+document.getElementById("hideDocs").addEventListener("click", function() {
+	document.getElementById("docs").style.display = "none";
+    if (document.getElementById("waiting").style.display === "") {
+		document.getElementById("results").style.display = "none";
+    } else {
+		document.getElementById("results").style.display = "";
+	}
+    document.getElementById("error").style.display = "none";
 }); 
