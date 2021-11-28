466d00f1f46455e2b8e4a9036014cc11674612ba
Mark Eaton <MarkEEaton@users.noreply.github.com>
2021-11-27 20:50:34 -0500

    parse spacy config successfully

--- cloud_function.py
+++ cloud_function.py
@@ -1,10 +1,9 @@
 import os
 import spacy
-from configparser import ConfigParser
 from flask import Response, request
 from google.cloud import storage
+from thinc.api import Config
 
-nlp = spacy.load("en_core_web_md", disable=["tagger", "parser", "ner", "lemmatizer"])
 
 def doaj_trio(request):
     try:
@@ -14,20 +13,22 @@ def doaj_trio(request):
         client = storage.Client()
         bucket = client.get_bucket(os.environ['bucket'])
 
-        config_blob = bucket.get_blob('config.cfg')
+        config_blob = bucket.get_blob(os.environ['config'])
         config_text = config_blob.download_as_text()
-        config = ConfigParser(allow_no_value=True)
-        config.read_string(config_text)
-
-        lang_cls = spacy.util.get_lang_class(config["nlp"]["lang"])
-        nlp = lang_cls.from_config(config)
-        user = nlp.from_bytes(user_nlp)
+        config = Config().from_str(config_text)
 
         blob_object = bucket.get_blob(blob)
         journal_nlp = blob_object.download_as_bytes()
-        journal = nlp.from_bytes(journal_nlp)
-        sim = user.similarity(journal)
+
+        lang_cls = spacy.util.get_lang_class(config["nlp"]["lang"])
+        nlp1 = lang_cls.from_config(config)
+        nlp2 = lang_cls.from_config(config)
+        nlp1.from_bytes(user_nlp)
+        nlp2.from_bytes(journal_nlp)
+
+        sim = nlp2.similarity(nlp1)
         return str(sim)
 
     except:
         raise
+
